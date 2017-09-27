409aeea2456e0c69e61f65a1f938a8af967a71f0
John Tuttle <thejwt@gmail.com>
2017-09-27 20:14:32 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,9 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    @brand = brand
-    BRANDS << brand
+    @brand = brand  
     if BRANDS.include?(brand) == false
+      BRANDS << brand
     end
   end
 
